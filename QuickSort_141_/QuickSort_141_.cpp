6adcbb23e5f4734f79055272fd3e552f736ce025
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-03-30 15:55:34 +0700

    Menambahkan if low pada void q_short

--- QuickSort_141_/QuickSort_141_.cpp
+++ QuickSort_141_/QuickSort_141_.cpp
@@ -58,8 +58,27 @@ void q_short(int low, int high)
         }
         cmp_count++;
 
+        while ((arr[1] <= pivot) && (low))
+        {
+            j--;
+            cmp_count++;
+        }
+        cmp_count++;
+
+        if (i < j)
+        {
+            swap(i, j);
+            mov_count++;
+        }
+    }
+    if (low < j)
+    {
+        swap(low, j);
+        mov_count++;
 
     }
+    q_short(low, j - 1);
+    q_short(j + 1, high);
 }
 
 
